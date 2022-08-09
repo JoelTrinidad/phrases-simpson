0685524300193cf915388fdda8e7fea9beb7b5ef
Joel Trinidad <jth_2929@hotmail.com>
2022-08-09 14:41:02 -0500

    Welcome component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Container from "./components/Container";
 import Header from "./components/Header";
+import Welcome from "./components/Welcome";
 
 function App() {
 
   return (
     <Container>
       <Header/>
+      <Welcome/>
     </Container>
   )
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import Logo from "./../assets/the-simpson.png";
+
+export default function Welcome() {
+  return (
+    <ContentLogo>
+      <WelcomeText>
+        Bienvenido, para obtener las frases haz click sobre la imagen
+      </WelcomeText>
+      <ImagenLogo src={Logo} alt="Logo Simpson" />
+    </ContentLogo>
+  );
+}
+
+const ImagenLogo = styled.img`
+  width: 450px;
+  height: auto;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const ContentLogo = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
+const WelcomeText = styled.p`
+  color: #2a9d8f;
+  text-align: center;
+  font-weight: 900;
+  padding: 50px 0 10px 0;
+  font-size: 25px;
+`;
