966d54411757be9aae2a4a5b7a928ba201b5a4bb
Joel Trinidad <jth_2929@hotmail.com>
2022-08-09 16:04:37 -0500

    data fetched from api

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import Header from "./components/Header";
 import Welcome from "./components/Welcome";
 
 function App() {
-
+  const reqApi = async () => {
+    const req = await fetch("https://simpsons-quotes-api.herokuapp.com/quotes?count=3");
+    const frases = await req.json();
+  }
   return (
     <Container>
       <Header/>
