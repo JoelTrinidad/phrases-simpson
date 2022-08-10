56efb4773f0f7962a65ce4b609b23a770e4ff7b8
Joel Trinidad <jth_2929@hotmail.com>
2022-08-10 10:59:21 -0500

    refresh button added

--- src/components/CharacterContainer.jsx
+++ src/components/CharacterContainer.jsx
@@ -2,17 +2,44 @@ import styled from "styled-components";
 import Character from "./Character";
 
 export default function CharacterContainer(props) {
-  const { characters } = props;
+  const { characters, reqApi } = props;
 
   return (
-    <ContainerCharacter>
-      {characters.map((character, index) => (
-        <Character dataCharacter={character} key={index} />
-      ))}
-    </ContainerCharacter>
+    <>
+      <ContainerCharacter>
+        {characters.map((character, index) => (
+          <Character dataCharacter={character} key={index} />
+        ))}
+      </ContainerCharacter>
+      <ContainerButton>
+        <Button onClick={reqApi}>Recargar Frases</Button>
+      </ContainerButton>
+    </>
   );
 }
 
 const ContainerCharacter = styled.div`
   display: flex;
+`;
+
+const ContainerButton = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  width: 250px;
+  background-color: #e76f51;
+  color: #fff;
+  border: none;
+  padding: 15px 20px;
+  border-radius: 25px;
+  font-size: 18px;
+  margin: 40px;
+  transition: all 0.2 ease-out;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #e77d63;
+  }
 `;
